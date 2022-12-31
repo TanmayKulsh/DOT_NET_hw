6574cc74618d0bae76e5e2f6554139732630655a
TanmayKulsh <tanmaykulsh@gmail.com>
2022-12-31 15:54:51 +0530

    adding update

--- DAY_2/Ecommerce/HR/SalesEmployee.cs
+++ DAY_2/Ecommerce/HR/SalesEmployee.cs
@@ -6,6 +6,36 @@ public class SalesEmployee:Employee{
     public float Incentive{get;set;}
 
 
-    public SalesEmployee():
+    public SalesEmployee():base(){
+
+    }
+    public SalesEmployee(float Target,float SalesDone,float Incentive,int id, string firstName, string lastName,
+                      string email, string contactNumber, string location,
+                      DateTime bDate, float da, float hra, float bsal):base(id,firstName,lastName,
+                      email,contactNumber,location,
+                       bDate,da,hra,bsal){
+
+                        this.Target = Target;
+                        this.SalesDone = SalesDone;
+                        this.Incentive = Incentive;
+    }
+
+    public void DoWork(){
+        if(SalesDone > Target){
+        this.Incentive = 5000;
+        }else{
+            this.Incentive = 0;
+        }
+    }
+
+    public override float ComputePay()
+    {
+        return base.ComputePay() + Incentive;
+    }
+    public override string ToString(){
+       
+        return base.ToString+ "Incentive =" +Incentive + ", Tartget="+ Target;
+    }
+
 
 }
